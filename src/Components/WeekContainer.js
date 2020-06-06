df0f9a2ebb21a320e63bf3607579e98cc2cd0c38
dpatel0114 <1991dharapatel@gmail.com>
2020-06-06 14:41:40 -0400

    first commit

---
+++ src/Components/WeekContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class WeekContainer extends Component {
+    render() {
+        return (
+            <div>
+                <h1> weather app </h1>
+            </div>
+        );
+    }
+}
+
+export default WeekContainer;
